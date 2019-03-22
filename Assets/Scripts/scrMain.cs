d487cef123b0eee8cb3553478c246783ada28ca0
unknown <33537@ETECAF.LOCAL>
2019-03-22 17:16:52 -0300

    first commit

---
+++ Assets/Scripts/scrMain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrMain : MonoBehaviour {
+
+    public KeyCode tecla;
+    bool critical;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(tecla))
+        {
+          var dmg = dano(63, 50, 20);
+          if (critical)
+          {
+              dmg = dmg * 2;
+          }
+          Debug.Log(dmg);
+        }
+	}
+    void modificador()
+    {
+
+    }
+
+    float dano(int lvl, int atk, int poder, float mod = 1)
+    {
+        return (((((2 * lvl) / 5) + 2 * poder * atk) / 50) + 2) * mod;
+    }
+}
---
+++ Assets/Scripts/scrMaintwo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrMaintwo : MonoBehaviour {
+
+    public int n = 0;
+    float d;
+	// Use this for initialization
+	void Start () {
+        d = n % 2;
+        if (d == 0)
+        {
+            if(n > 2 && n < 5)
+            {
+                Debug.Log("não estranho");
+            }
+            else
+            {
+                if (n > 6 && n < 20)
+                {
+                    Debug.Log("Estranho");
+                }
+                else
+                {
+                    if (n>20)
+                    {
+                        Debug.Log("não estranho");
+                    }
+                    else
+                    {
+                        Debug.Log("Estranho");
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Estranho");
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
